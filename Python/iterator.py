50baf6628f076348dcb9bc18d2a913194df16549
Tim Zhang <tmzpanda@gmail.com>
2023-03-14 16:13:36 -0400

    Update iterator.py

--- Python/iterator.py
+++ Python/iterator.py
@@ -1,8 +1,34 @@
 # iterator 
 
 
+__iter__
+my_iterator = iter(my_list)
 
+
+__next__
+next(my_iterator))
 
 
 
 # generator
+generator_1 = (x*x for x in range(3))
+generator_1
+
+def func(n):
+    for x in range(n):
+        yield x*x
+        
+generator_2 = func(3)
+generator_2
+
+next(generator_2)
+
+
+
+
+
+
+# hasNext() and next()
+
+
+
