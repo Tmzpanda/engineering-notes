e6ca4f95f381e91b5414193d9a01b3f8d8ece50e
Tim Zhang <tmzpanda@gmail.com>
2023-03-17 23:11:50 -0400

    Create oop.go

---
+++ Go/oop.go
@@ -0,0 +1,37 @@
+// base class
+type Product struct {
+    name  string
+    price float64
+}
+
+
+// inheritance
+type Clothing struct {
+    *Product
+    size  string
+    color string
+}
+
+func (c *Clothing) getDiscountedPrice() float64 {
+    if c.size == "S" {
+        return c.price * 0.9
+    }
+    return c.price * 0.8
+}
+
+
+// interface
+type PaymentMethod interface {
+    makePayment(amount float64)
+}
+
+
+// implementation
+type CreditCard struct {
+    cardNumber  string
+    expiryDate  string
+}
+
+func (cc *CreditCard) makePayment(amount float64) {
+    fmt.Printf("Processing credit card payment for $%.2f\n", amount)
+}
