b6900d0fc35c51fb53e9a9dbb4ef144a726a7dd2
Tim Zhang <tmzpanda@gmail.com>
2023-08-26 12:20:40 -0700

    Update and rename DDL.md to DDL.sql

--- Hive/DDL.sql
+++ Hive/DDL.sql
@@ -1,7 +1,6 @@
-```sql
+-- DDL
 CREATE TABLE page_view(viewTime INT, userid BIGINT, page_url STRING, referrer_url STRING, ip STRING COMMENT 'IP Address of the User') 
 PARTITIONED BY (dt STRING, country STRING)   -- partition
 CLUSTERED BY(userid) SORTED BY(viewTime) INTO 32 BUCKETS   -- bucket
 STORED AS SEQUENCEFILE;  -- format
 
-```
