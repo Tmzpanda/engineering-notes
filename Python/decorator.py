8659d2cb004d7158994353bf63751cbdf713078d
Tim Zhang <tmzpanda@gmail.com>
2023-08-26 10:46:55 -0700

    Update decorator.py

--- Python/decorator.py
+++ Python/decorator.py
@@ -1,40 +1,25 @@
-# decorator
-
-# fibonacci
+# memoization
 def fib(n: int) -> int:
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    memo = {}
+    def dfs(n):
+        # memo
+        if n in memo:
+            return memo[n]
+        # base
+        if n == 0 or n == 1:
+            return n
 
-    return fib(n-1) + fib(n-2)
-  
-  
-# fibonacci with memoization
-def fib(n: int) -> int:
-  
-    def dfs(n: int, memo: dict) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-
-        if n not in memo:
-            memo[n] = dfs(n - 1, memo) + dfs(n - 2, memo)
-        
+        memo[n] = dfs(n - 1) + dfs(n - 2)
         return memo[n]
-
-    return dfs(n, {})
-
+    
+    return dfs(n)
 
 # decorator syntax sugar
 from functools import lru_cache
 @lru_cache(maxsize=None)
 def fib(n: int) -> int:
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    if n == 0 or n == 1:
+        return n
 
     return fib(n-1) + fib(n-2)
   
@@ -42,22 +27,19 @@ def fib(n: int) -> int:
 # custom decorator
 def decorator(func): 
     cache = {}
-
+    
     def wrapper(n):   
-        if n not in cache:
-            cache[n] = func(n)
+        if n in cache:
+            return cache[n]
+            
+        cache[n] = func(n)
         return cache[n]
 
-    return wrapper 
+    return wrapper     
 
 @decorator
 def fib(n: int) -> int:
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    if n == 0 or n == 1:
+        return n
 
     return fib(n - 1) + fib(n - 2)
-
-
-
