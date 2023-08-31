9868aae702ff4d8bd79cbb2d7de05d004db67a78
Tim Zhang <tmzpanda@gmail.com>
2023-08-31 15:24:58 -0700

    Update pyspark.py

--- Spark/pyspark.py
+++ Spark/pyspark.py
@@ -1,11 +1,12 @@
 # SparkSession
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, explode, from_json, concat_ws, lit
+from pyspark.sql.types import StructType, StructField, StringType
 
 spark = SparkSession.builder \
     .master("local[1]") \
     .appName("spark demo") \
     .getOrCreate() 
-
 
 # create spark df
 data = [["James","Smith",30,"M"], 
@@ -19,159 +20,81 @@ df = spark.createDataFrame(data=data, schema=columns)
 
 
 # read from csv
-df = spark.read.csv("/tmp/resources/zipcodes.csv")
-
+df = spark.read.csv('filename.csv', header=True, inferSchema=True)
 
 # explore
 df.printSchema()
 df.select('col').show(5)
 df.drop('col)
-
+df['survey_categoty'].distinct
+df.select('survey_type', 'survey_category').distinct().show(100, False)
 
 # clean
-df.filter(df.age > 40)
-df = df.withColumn("age_square", col("age")**2)
-df.filter(df.native_country != 'Holand-Netherlands')
-
-
-
-# aggregate
-df.filter(df.native_country == 'Holand-Netherlands').count()
-df.groupBy("education").count().sort("count",ascending=True).show()
-df.groupby('marital').agg({'capital_gain': 'mean'}).show()
-df.groupby('native_country').agg({'native_country': 'count'}).sort(asc("count(native_country)")).show()
-
-
-
-# feature engineer
-# onehotencoder
-
-
-
-
-#e.g. Spark transforms the unstructured logs, applies schema and insert into target table, 
-# transformations includes cleansing (e.g. deduplicate, rename, cast, json explode, filter), enrichment (join, aggregations), etc.
-
-
-# Read the CSV file into a PySpark DataFrame
-df = spark.read.csv('filename.csv', header=True, inferSchema=True)
-
 # Trim the spaces from the column names
 df = df.toDF(*[c.strip() for c in df.columns])
 
 # Trim the spaces from the column values
 df = df.select([trim(c).alias(c) if c.dtype == 'string' else c for c in df.columns])
 
+# deduplicate
+df = df.dropDuplicates(["col1", "col2"])
+
+# transform
+df_transformed = df.select(col("id"),
+                           concat_ws(' ', col("first_name"), col("second_name")).alias("name"),
+                           (col("floor") * lit(steps_per_floor)).alias("steps_to_desk"))
+
+# rename
+df = df.withColumnRenamed("old_col", "new_col")
+
+# add a column
+df.withColumn('advantage_login_date_1', to_date(coalesce(fma.advantage_login_date, lit('1900-01-01')),"yyyy-MM-dd"))
+
+# cast
+df = df.withColumn("new_col", col("old_col").cast("integer"))
+
+# json explode
+# pattern matching
+json_schema = StructType([
+    StructField("key", StringType()), 
+    StructField("value", StringType())
+    ])
+df = df.withColumn("exploded", explode(from_json("json_column", json_schema)))
+
+
+# filter
+df = df.filter(col("column_name") > 10)
+df = df.filter(df.native_country != 'Holand-Netherlands')
+df.filter("survey_di = 'bcbssc").groupBy('survey_id').count.show(10,false)
+
+# regex
+df = df.filter(expr("column_name LIKE 'pattern%'"))
+
+
+
+# join
+df = df.join(df_dim, on="common_column", how="inner")
+
+# aggregate
+df.filter(df.native_country == 'Holand-Netherlands').count()
+df.groupBy('partner', 'client_hierarchy').count().sort(desc('count'))
+df.groupBy("education").count().sort("count",ascending=True).show()
+df.groupby('marital').agg({'capital_gain': 'mean'}).show()
+df.groupby('native_country').agg({'native_country': 'count'}).sort(asc("count(native_country)")).show()
+df = df.groupBy("group_column").agg(sum("numeric_column"), avg("numeric_column"), max("numeric_column"))
 # Find the highest salary for each department
 highest_salaries = df.groupBy('department').agg(max('salary').alias('highest_salary'))
-
-# Show the results
 highest_salaries.show()
 
 
+# load 
+
+df_transformed\
+    .write\
+    .csv("resources/loaded_data", mode="overwrite", header=True)
+
+# feature engineer
+# onehotencoder
 
 
-
-
-
-
-
-from sparksql.context import spark_session
-from pyspark.sql.functions import col, concat_ws,lit
-import json
-
-
-def process():
-    # extract
-    df = spark_session \
-        .read \
-        .parquet("resources/employees")
-    df.show(5)
-
-    # transform
-    with open("resources/etl_config.json", 'r') as config_file:
-        config_dict = json.load(config_file)
-    steps_per_floor = config_dict["steps_per_floor"]
-
-    df_transformed = df.select(col("id"),
-                               concat_ws(' ', col("first_name"), col("second_name")).alias("name"),
-                               (col("floor") * lit(steps_per_floor)).alias("steps_to_desk"))
-    df_transformed.show(5)
-
-    # load
-    df_transformed\
-        .write\
-        .csv("resources/loaded_data", mode="overwrite", header=True)
-
-
-if __name__ == '__main__':
-    process()
-    
----
-
-# df
-
-df = spark.read.table('edw.dim_survey_definition')
-df.printSchema()
-
-hadoop fs -ls /prod/data/etl/mongo_extracts/missions/missionInstance/year=2022/month=01/
-df_1 = spark.read.parquet("/prod/data/etl/mongo_extracts/missions/missionInstance/year=2022/month=01/day=28")
-
-hadoop fs -ls /prod/data/etl/mongo_extracts/survey/surveyQuestionDefinition/ 
-df = spark.read.json("/prod/data/etl/mongo_extracts/survey/surveyQuestionDefinition/")
-
-
-df['survey_categoty'].distinct
-df.select('survey_type', 'survey_category').distinct().show(100, False)
-
-df.filter("survey_di = 'bcbssc").groupBy('survey_id').count.show(10,false)
-df.groupBy('partner', 'client_hierarchy').count().sort(desc('count'))
-
-
-df.withColumn('advantage_login_date_1', to_date(coalesce(fma.advantage_login_date, lit('1900-01-01')),"yyyy-MM-dd"))
-
-
-
-df.write.saveAsTable('edw_tim.stg_stats')
-
-df.coalesce(1).
-    write.
-    option("sep","|").
-    option("header","true").
-    csv("dsd.csv")
-
-
-
-
-----------------------------------------------------------------------------------------------------------------
-# console
-
-spark2-submit \
---executor-memory 5g \
---conf spark.dynamicAllocation.minExecutors=5 \
---conf spark.dynamicAllocation.enabled=true \
---conf spark.dynamicAllocation.maxExecutors=10 \
---conf spark.yarn.executor.memoryOverhead=1024 \
---executor-cores 5 \
---driver-memory 5g \
-agg_daily_summary.py \
---db edw --etl-start-date 2021-11-01 \
---etl-end-date 2021-11-16
-
-
-
-pyspark2 --executor-memory 10g \
---executor-cores 5 \
---conf spark.dynamicAllocation.enabled=true \
---conf spark.dynamicAllocation.minExecutors=5 \
---conf spark.dynamicAllocation.maxExecutors=10 \
---conf spark.hadoop.hive.exec.dynamic.partition=true
-
-
-
-pyspark2 --executor-memory 10g --conf spark.dynamicAllocation.enabled=true --conf spark.dynamicAllocation.minExecutors=10 --conf spark.dynamicAllocation.maxExecutors=20 --conf spark.hadoop.hive.exec.dynamic.partition=true --conf spark.hadoop.hive.exec.dynamic.partition.mode=nonstrict
---conf spark.hadoop.hive.exec.max.dynamic.partitions=1200 
-
-
-----------------------------------
 
